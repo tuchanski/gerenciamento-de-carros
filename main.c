cb59be4fb46dbb2b55bf2d617a2479fd20bca613
Tuchanski <gui.rocha2004@gmail.com>
2024-06-15 22:48:11 -0300

    Cria projeto com struct do carro

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef struct
+{
+    char marca[20];
+    char modelo[20];
+    int anoFabricacao;
+    int kilometragem;
+    float preco;
+} Carro;
+
+int main(void) {
+
+    return 0;
+}
