0425a0643bcddf894a70420953b22290a7cdacba
Tuchanski <gui.rocha2004@gmail.com>
2024-06-17 09:26:11 -0300

    Adiciona lógica de lista encadeada

--- main.c
+++ main.c
@@ -13,21 +13,87 @@ typedef struct
     float preco;
 } Carro;
 
-int main(void) {
+typedef struct No
+{
+    Carro carro;
+    struct No *proximo;
+} No;
 
-    FILE *fileRead = fopen("../dados/dados.txt", "r");
-
-    if (fileRead == NULL){
-        printf("\nErro: não foi possível efetuar a leitura de dados.txt");
+No *criarNo(Carro carro)
+{
+    No *novoNo = (No *)malloc(sizeof(No));
+    if (novoNo != NULL)
+    {
+        novoNo->carro = carro;
+        novoNo->proximo = NULL;
     }
-    else{
-        printf("Leuu");
-        
-        Carro carro;
-        strcpy(carro.marca, "Honda");
-        printf("\n\nMarca: %s", carro.marca);
-    }
-
-    return 0;
+    return novoNo;
 }
 
+void inserirCarro(No **cabeca, Carro novoCarro)
+{
+    No *novoNo = criarNo(novoCarro);
+    if (novoNo == NULL)
+    {
+        printf("\nErro: não foi possivel alocar memória.");
+        return;
+    }
+
+    if (*cabeca == NULL || (*cabeca)->carro.preco > novoCarro.preco)
+    {
+        novoNo->proximo = *cabeca;
+        *cabeca = novoNo;
+    }
+    else
+    {
+        No *atual = *cabeca;
+        while (atual->proximo != NULL && atual->proximo->carro.preco <= novoCarro.preco)
+        {
+            atual = atual->proximo;
+        }
+        novoNo->proximo = atual->proximo;
+        atual->proximo = novoNo;
+    }
+}
+
+void imprimirLista(No *no)
+{
+    while (no != NULL)
+    {
+        printf("Marca: %s\n", no->carro.marca);
+        printf("Modelo: %s\n", no->carro.modelo);
+        printf("Ano de Fabricação: %d\n", no->carro.anoFabricacao);
+        printf("Kilometragem: %d\n", no->carro.kilometragem);
+        printf("Preço: %.2f\n\n", no->carro.preco);
+        no = no->proximo;
+    }
+}
+
+int main(void)
+{
+    FILE *fileRead = fopen("../dados/dados.txt", "r");
+
+    if (fileRead == NULL)
+    {
+        printf("\nErro: não foi possível efetuar a leitura de dados.txt");
+        return 1;
+    }
+    else
+    {
+        No *cabeca = NULL;
+
+        Carro car1 = {"Toyota", "Corolla", 2015, 60000, 50000.00};
+        Carro car2 = {"Honda", "Civic", 2018, 30000, 75000.00};
+        Carro car3 = {"Ford", "Focus", 2012, 90000, 35000.00};
+        Carro car4 = {"Chevrolet", "Cruze", 2020, 15000, 70000.00};
+
+        inserirCarro(&cabeca, car1);
+        inserirCarro(&cabeca, car2);
+        inserirCarro(&cabeca, car3);
+        inserirCarro(&cabeca, car4);
+
+        imprimirLista(cabeca);
+
+        return 0;
+    }
+}
